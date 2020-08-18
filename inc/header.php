f314de774ec69d3512fb87a3e4f2b6d0f79c1760
Madero Padero <k1721863@kingston.ac.uk>
2020-08-19 00:05:26 +0100

    Sorting is visalised and delayed

--- inc/header.php
+++ inc/header.php
@@ -14,12 +14,24 @@
   <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow">
       <h5 class="my-0 mr-md-auto font-weight-normal">Sorting algorithm visualisations</h5>
       <nav class="my-2 my-md-0 mr-md-3">
-        <a class="p-2 text-dark" href="#">Features</a>
-        <a class="p-2 text-dark" href="#">Enterprise</a>
-        <a class="p-2 text-dark" href="#">Support</a>
-        <a class="p-2 text-dark" href="#">Pricing</a>
+        <div class="dropdown">
+          <button id="my-dropdown" class="btn btn-primary dropdown-toggle" 
+          data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Algorithm</button>
+          <div class="dropdown-menu" aria-labelledby="my-dropdown">
+            <a class="dropdown-item" href="#">Bubble sort</a>
+            <a class="dropdown-item" href="#">Merge sort</a>
+            <a class="dropdown-item" href="#">Quick sort</a>
+            <a class="dropdown-item" href="#">Heap sort</a>
+          </div>
+          <a class="btn btn-outline-primary" href="#">Speed</a>
+          <a class="btn btn-outline-primary" href="#">Array size</a>
+          <a class="btn btn-outline-primary" href="#">Generate new array</a>
+          <a class="btn btn-outline-danger" href="#">Sort!</a>
+
+ 
+        </div>
+       
       </nav>
-      <a class="btn btn-outline-primary" href="#">Sign up</a>
     </div>
 
 <div class="container-flex p-4">
